2d3ea9379792905d93a76d16709ebb3e327e2979
Akshat Gupta <144452203+Akshat3144@users.noreply.github.com>
2024-02-27 19:57:01 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,27 +1,4 @@
-{
-    "version": 2,
-    "builds" : [
-        {
-            "src":"myproject/wsgi.py",
-            "use":"@vercel/python",
-            "config":{"maxLamdaSize":"15mb","runtime":"python3.9"}
-        },
-        {
-            "src":"build_files.sh",
-            "use":"@vercel/static-build",
-            "config":{
-                "distDir":"staticfiles_build"
-            }
-        }
-    ],
-    "routes":[
-        {
-            "src":"/static/(.*)",
-            "dest":"/static/$1"
-        },
-        {
-            "src":"/(.*)",
-            "dest":"myproject/wsgi.py"
-        }
-    ]
-}
+echo " BUILD START"
+python3.9 -m pip install - r requirements.txt
+python3.9 manage.py collectstatic  --noinput --clear
+echo " BUILD END"
